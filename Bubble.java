6af3341fa0639237f7e17bb3a135633515c7caad
Pushpendra Kumar Ahirwar <66326261+Pushpendra-Kumar-Ahirwar@users.noreply.github.com>
2022-07-19 00:16:18 +0530

    Add files via upload

---
+++ Bubble.java
@@ -0,0 +1,22 @@
+public class Bubble {
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 2, 1, 3, 6, 9, 10 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printarr(arr);
+    }
+
+}
